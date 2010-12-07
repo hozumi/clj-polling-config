42724e85cf4b9d8c702845a64442d96e180d96ef
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2010-12-08 00:05:59 +0900

    add callback to run

--- src/hozumi/polling_config.clj
+++ src/hozumi/polling_config.clj
@@ -5,12 +5,17 @@
 (def config (ref nil))
 (def last-modified (ref 0))
 
-(defn run [file interval-msec]
-  (loop [file (javaio/file file)]
-    (let [current-last-modified (.lastModified file)]
-      (when (> current-last-modified @last-modified)
-	(dosync
-	 (ref-set last-modified current-last-modified)
-	 (ref-set config (-> file slurp yaml/parse-string))))
-      (Thread/sleep interval-msec)
-      (recur file))))
+(defn run
+  ([file interval-msec]
+     (run file interval-msec nil))
+  ([file interval-msec callback]
+     (loop [file (javaio/file file)]
+       (let [current-last-modified (.lastModified file)]
+	 (when (> current-last-modified @last-modified)
+	   (dosync
+	    (ref-set last-modified current-last-modified)
+	    (ref-set config (-> file slurp yaml/parse-string))
+	    (when callback
+	      (callback))))
+	 (Thread/sleep interval-msec)
+	 (recur file)))))
