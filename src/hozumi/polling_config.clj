14111849c8fe505a9c36c539e2212ecf512ae9ae
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2010-12-08 14:44:46 +0900

    add set

--- src/hozumi/polling_config.clj
+++ src/hozumi/polling_config.clj
@@ -1,21 +1,33 @@
 (ns hozumi.polling-config
+  (:refer-clojure :exclude [set])
   (:require [clj-yaml.core :as yaml]
 	    [clojure.java.io :as javaio]))
 
 (def config (ref nil))
+(def file (ref nil))
 (def last-modified (ref 0))
 
+(defn set
+  ([f]
+     (dosync
+      (ref-set file (javaio/file f)))
+      (ref-set last-modified (.lastModified @file))
+      (ref-set config (-> @file slurp yaml/parse-string))))
+
 (defn run
-  ([file interval-msec]
-     (run file interval-msec nil))
-  ([file interval-msec callback]
-     (loop [file (javaio/file file)]
-       (let [current-last-modified (.lastModified file)]
+  ([interval-msec]
+     (run @file interval-msec))
+  ([f interval-msec]
+     (run f interval-msec nil))
+  ([f interval-msec callback]
+     (dosync (ref-set file (javaio/file f)))
+     (loop []
+       (let [current-last-modified (.lastModified @file)]
 	 (when (> current-last-modified @last-modified)
 	   (dosync
 	    (ref-set last-modified current-last-modified)
-	    (ref-set config (-> file slurp yaml/parse-string))
+	    (ref-set config (-> @file slurp yaml/parse-string))
 	    (when callback
 	      (callback @config))))
 	 (Thread/sleep interval-msec)
-	 (recur file)))))
+	 (recur)))))
