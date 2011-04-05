7cc5f354626330802ebc9e83f9441914d50a1326
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2011-04-05 13:31:21 +0900

    update config immediately

--- src/hozumi/polling_config.clj
+++ src/hozumi/polling_config.clj
@@ -6,14 +6,17 @@
      (run-background file interval-msec (ref nil)))
   ([file interval-msec conf-ref]
      (let [file (javaio/file file)
-           last-modified (ref 0)]
+           last-modified (ref 0)
+           update-config (fn []
+                           (let [current-last-modified (.lastModified file)]
+                             (when (not= current-last-modified @last-modified)
+                               (dosync
+                                (ref-set last-modified current-last-modified)
+                                (ref-set conf-ref (-> file slurp read-string))))))]
+       (update-config)
        (future
         (loop []
-          (let [current-last-modified (.lastModified file)]
-            (when (> current-last-modified @last-modified)
-              (dosync
-               (ref-set last-modified current-last-modified)
-               (ref-set conf-ref (-> file slurp read-string)))))
+          (update-config)
           (Thread/sleep interval-msec)
           (recur)))
        conf-ref)))
